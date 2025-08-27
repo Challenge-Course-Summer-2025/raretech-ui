46b6636a4c308d7fd0efcb1357c287f6b76533bb
Marstach-svg <ryoken.0325@gmail.com>
2025-08-27 21:24:34 +0900

    手動投稿バッジ削除

--- src/components/PostHistory.jsx
+++ src/components/PostHistory.jsx
@@ -5,9 +5,6 @@ const PostHistory = ({ postHistory }) => {
 		<section className="bg-white border border-sky-200 rounded-lg shadow-md p-6">
 			<h3 className="text-md font-semibold text-blue-900 mb-4 flex items-center gap-2 pl-3">
 				📝 投稿履歴一覧
-				<span className="bg-amber-500 text-blue-900 text-xs font-bold p-2 rounded-md">
-					手動投稿
-				</span>
 			</h3>
 
 			{postHistory.map((post) => (
