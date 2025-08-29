fcb612fd263ad47001af4dd0d1aaf84becb57f59
sasakifuruta <rixiaozig@gmail.com>
2025-08-29 23:52:44 +0900

    update Header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,16 +4,16 @@ const Header = () => {
 		<header className="bg-blue-800 text-white p-4 shadow-md">
 			<div className="container mx-auto flex justify-between items-center">
 				{/* ロゴとタイトル部分 */}
-				<div className="flex items-center space-x-3 mask-radial-at-center">
-					<div>
-						<a href="/">
+				<a href="/">
+					<div className="flex items-center space-x-3 mask-radial-at-center">
+						<div>
 							<img src={RareTech} alt="RareTECHLogo" className="h-10 w-auto" />
-						</a>
+						</div>
+						<div>
+							<h1 className="text-2xl font-bold">RareTECH</h1>
+						</div>
 					</div>
-					<div>
-						<h1 className="text-2xl font-bold">RareTECH</h1>
-					</div>
-				</div>
+				</a>
 			</div>
 		</header>
 	);
